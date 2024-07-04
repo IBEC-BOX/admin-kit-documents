6519a2e3086245346a57a709acd3f7ad68da07c3
Anastas Mironov <ast@l80.ru>
2024-07-05 02:01:54 +0900

    fix: laravel-data version ^4.5

--- src/UI/API/Data/DocumentData.php
+++ src/UI/API/Data/DocumentData.php
@@ -4,12 +4,15 @@ namespace AdminKit\Documents\UI\API\Data;
 
 use AdminKit\Documents\Models\Document;
 use Spatie\LaravelData\Attributes\MapName;
+use Spatie\LaravelData\Concerns\WithDeprecatedCollectionMethod;
 use Spatie\LaravelData\Data;
 use Spatie\LaravelData\Mappers\SnakeCaseMapper;
 
 #[MapName(SnakeCaseMapper::class)]
 class DocumentData extends Data
 {
+    use WithDeprecatedCollectionMethod;
+
     public function __construct(
         public string $title,
         public ?string $link,
