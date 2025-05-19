e193685ff47314c1d2f4c51310b3f6bbbe0aae40
Anastas Mironov <anastas.mironov@freedompay.kz>
2025-05-19 16:41:21 +0500

    feat: add custom_title attribute

--- src/Models/Document.php
+++ src/Models/Document.php
@@ -37,6 +37,13 @@ class Document extends AbstractModel implements HasMedia
         return $this->belongsTo(DocumentCategory::class);
     }
 
+    public function getCustomTitleAttribute(): string
+    {
+        $title = $this->getTranslations('title');
+
+        return $title['ru'] ?? $title['kk'] ?? $title['en'] ?? '-';
+    }
+
     public function scopeYear(Builder $query, $year): Builder
     {
         $date = Carbon::createFromDate($year);
